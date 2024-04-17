a9b85cf89494cf3335c73a5bcb6947fe7c78887f
Adrien Brault <adrien.brault@gmail.com>
2024-04-17 11:12:35 +0200

    test: fix InstructriceTest

---
+++ tests/Fixtures/Person.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdrienBrault\Instructrice\Tests\Fixtures;
+
+class Person
+{
+    public function __construct(
+        public string $name
+    ) {
+    }
+}
--- tests/InstructriceTest.php
+++ tests/InstructriceTest.php
@@ -5,101 +5,58 @@ declare(strict_types=1);
 namespace AdrienBrault\Instructrice\Tests;
 
 use AdrienBrault\Instructrice\Instructrice;
-use AdrienBrault\Instructrice\InstructriceFactory;
 use AdrienBrault\Instructrice\LLM\LLMInterface;
+use AdrienBrault\Instructrice\Tests\Fixtures\Person;
+use ApiPlatform\JsonSchema\Schema;
+use ApiPlatform\JsonSchema\SchemaFactoryInterface;
 use Gioni06\Gpt3Tokenizer\Gpt3Tokenizer;
 use Gioni06\Gpt3Tokenizer\Gpt3TokenizerConfig;
-use Limenius\Liform\LiformInterface;
-use OpenAI\Responses\Chat\CreateResponse;
-use OpenAI\Responses\Meta\MetaInformation;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Serializer\Serializer;
 
 #[CoversClass(Instructrice::class)]
 class InstructriceTest extends TestCase
 {
-    public function test(): void
+    public function testDeserializeList(): void
     {
         $llm = $this->createMock(LLMInterface::class);
         $llm
             ->method('get')
             ->willReturn([
-                'name' => 'John',
-            ]);
-        $liform = $this->createMock(LiformInterface::class);
-        $liform
-            ->method('transform')
-            ->willReturn($fakeSchema = [
-                'type' => 'object',
-                'properties' => [
-                    'name' => [
-                        'type' => 'string',
-                    ],
+                'list' => [
+                    'name' => 'John',
                 ],
             ]);
+        $schemaFactory = $this->createMock(SchemaFactoryInterface::class);
+        $schemaFactory
+            ->method('buildSchema')
+            ->willReturn($schema = new Schema());
+        $serializer = $this->createMock(Serializer::class);
+        $serializer
+            ->method('denormalize')
+            ->with(
+                [
+                    'name' => 'John',
+                ],
+                Person::class . '[]'
+            )
+            ->willReturn($deserializedList = [new Person('John')]);
 
         $instructrice = new Instructrice(
-            InstructriceFactory::createFormFactory(),
-            $liform,
             $llm,
             new NullLogger(),
-            new Gpt3Tokenizer(new Gpt3TokenizerConfig())
+            new Gpt3Tokenizer(new Gpt3TokenizerConfig()),
+            $schemaFactory,
+            $serializer,
         );
 
-        $form = $instructrice->fillForm(
-            context: 'context',
-            newForm: fn (FormFactoryInterface $ff) => $ff->createBuilder()
-                ->add('name', TextType::class)
-                ->getForm(),
+        $list = $instructrice->deserializeList(
+            'context',
+            Person::class,
         );
 
-        $this->assertSame('John', $form->get('name')->getData());
-    }
-
-    public function mockOpenAiResponse(
-        string $content
-    ): CreateResponse {
-        return CreateResponse::from(
-            [
-                'id' => 'id',
-                'object' => 'object',
-                'created' => 0,
-                'model' => 'model',
-                'choices' => [
-                    [
-                        'index' => 0,
-                        'message' => [
-                            'role' => 'assistant',
-                            'content' => $content,
-                            'function_call' => null,
-                            'tool_calls' => null,
-                        ],
-                        'finish_reason' => null,
-                    ],
-                ],
-                'usage' => [
-                    'prompt_tokens' => 4,
-                    'completion_tokens' => 4,
-                    'total_tokens' => 8,
-                ],
-            ],
-            MetaInformation::from([
-                'x-request-id' => ['x-request-id'],
-                'openai-model' => ['openai-model'],
-                'openai-organization' => ['openai-organization'],
-                'openai-version' => ['openai-version'],
-                'openai-processing-ms' => ['openai-processing-ms'],
-                'x-ratelimit-limit-requests' => ['x-ratelimit-limit-requests'],
-                'x-ratelimit-limit-tokens' => ['x-ratelimit-limit-tokens'],
-                'x-ratelimit-remaining-requests' => ['x-ratelimit-remaining-requests'],
-                'x-ratelimit-remaining-tokens' => ['x-ratelimit-remaining-tokens'],
-                'x-ratelimit-reset' => ['x-ratelimit-reset'],
-                'x-ratelimit-reset-requests' => ['x-ratelimit-reset-requests'],
-                'x-ratelimit-reset-tokens' => ['x-ratelimit-reset-tokens'],
-            ])
-        );
+        $this->assertSame($deserializedList, $list);
     }
 }
