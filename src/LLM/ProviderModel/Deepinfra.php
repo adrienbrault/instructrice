b16cceb4780e866bc389c34b96f2d6fa9faea23f
Adrien Brault <adrien.brault@gmail.com>
2024-04-19 05:06:32 +0200

    feat: add Llama 3 on deepinfra

--- src/LLM/ProviderModel/Deepinfra.php
+++ src/LLM/ProviderModel/Deepinfra.php
@@ -14,6 +14,8 @@ enum Deepinfra: string implements ProviderModel
     case WIZARDLM2_22 = 'microsoft/WizardLM-2-8x22B';
     case WIZARDLM2_7 = 'microsoft/WizardLM-2-7B';
     case DBRX = 'databricks/dbrx-instruct';
+    case LLAMA3_8B = 'meta-llama/Meta-Llama-3-8B-Instruct';
+    case LLAMA3_70B = 'meta-llama/Meta-Llama-3-70B-Instruct';
 
     public function getApiKeyEnvVar(): ?string
     {
@@ -27,6 +29,7 @@ enum Deepinfra: string implements ProviderModel
             self::WIZARDLM2_22 => 64000,
             self::WIZARDLM2_7 => 32000,
             self::DBRX => 32000,
+            self::LLAMA3_8B, self::LLAMA3_70B => 8000,
         };
     }
 
@@ -37,12 +40,14 @@ enum Deepinfra: string implements ProviderModel
 
     public function getCost(): Cost
     {
-        return Cost::create(match ($this) {
-            self::MIXTRAL_22 => 0.65,
-            self::WIZARDLM2_22 => 0.65,
-            self::WIZARDLM2_7 => 0.1,
-            self::DBRX => 0.6,
-        });
+        return match ($this) {
+            self::MIXTRAL_22 => Cost::create(0.65),
+            self::WIZARDLM2_22 => Cost::create(0.65),
+            self::WIZARDLM2_7 => Cost::create(0.1),
+            self::DBRX => Cost::create(0.6),
+            self::LLAMA3_8B => Cost::create(0.1),
+            self::LLAMA3_70B => new Cost(0.59, 0.79),
+        };
     }
 
     public function getLabel(bool $prefixed = true): string
@@ -52,6 +57,8 @@ enum Deepinfra: string implements ProviderModel
             self::WIZARDLM2_22 => 'WizardLM 2 8x22B',
             self::WIZARDLM2_7 => 'WizardLM 2 7B',
             self::DBRX => 'DBRX',
+            self::LLAMA3_8B => 'Llama3 8B',
+            self::LLAMA3_70B => 'Llama3 70B',
         };
     }
 
