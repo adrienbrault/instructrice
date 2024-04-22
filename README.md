6612311bb8c9a1fee94dfaeaf57586671b01ca75
Adrien Brault <adrien.brault@gmail.com>
2024-04-22 23:16:06 +0200

    docs: oooas

--- README.md
+++ README.md
@@ -75,6 +75,9 @@ $label = $instructrice->get(
 assert($label === 'positive');
 ```
 
+You can also use third party json schema libraries like [goldspecdigital/oooas][oooas] to generate the schema:
+- [demo/oooas.php](demo/oooas.php)
+
 https://github.com/adrienbrault/instructrice/assets/611271/da69281d-ac56-4135-b2ef-c5e306a56de2
 
 ## Supported providers
---
+++ demo/oooas.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+use AdrienBrault\Instructrice\Instructrice;
+use AdrienBrault\Instructrice\InstructriceFactory;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+
+$demo = require __DIR__ . '/bootstrap.php';
+$demo(function (Instructrice $instructrice, ?string $context, ConsoleOutputInterface $output) {
+    $instructrice->get(
+        Schema::object()
+            ->properties(
+                Schema::string('name'),
+                Schema::string('dateOfBirth')->format(Schema::FORMAT_DATE_TIME),
+                Schema::boolean('isAlive'),
+                Schema::array('significantContributions')
+                    ->items(Schema::string()),
+            )
+            ->toArray(),
+        'Most impact scientists in history.',
+        onChunk: InstructriceFactory::createOnChunkDump($output->section()),
+    );
+});
+
+/*
+1 array:4 [
+  "name" => "Marie Curie"
+  "dateOfBirth" => "1867-11-07"
+  "isAlive" => false
+  "significantContributions" => array:3 [
+    0 => "Pioneered radioactivity research"
+    1 => "First woman to win a Nobel Prize"
+    2 => "First person to win two Nobel Prizes in different fields (Physics and Chemistry)"
+  ]
+]
+*/
