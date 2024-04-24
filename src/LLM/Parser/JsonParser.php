de9c74fd7c2c4d2c61e6494167db37a4b9a09f9f
Adrien Brault <adrien.brault@gmail.com>
2024-04-24 03:31:25 +0200

    fix: silence json decoding error

--- src/LLM/Parser/JsonParser.php
+++ src/LLM/Parser/JsonParser.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace AdrienBrault\Instructrice\LLM\Parser;
 
+use Exception;
 use GregHunt\PartialJson\JsonParser as GregHuntJsonParser;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 use function Psl\Regex\replace;
 
 class JsonParser implements ParserInterface
 {
     public function __construct(
-        private readonly GregHuntJsonParser $jsonParser = new GregHuntJsonParser()
+        private readonly GregHuntJsonParser $jsonParser = new GregHuntJsonParser(),
+        private readonly LoggerInterface $logger = new NullLogger(),
     ) {
     }
 
@@ -34,7 +38,16 @@ class JsonParser implements ParserInterface
         }
 
         if (str_starts_with($content, '{') || str_starts_with($content, '[')) {
-            $data = $this->jsonParser->parse($content);
+            try {
+                $data = $this->jsonParser->parse($content);
+            } catch (Exception $e) {
+                $this->logger->warning('Failed to parse JSON', [
+                    'content' => $content,
+                    'exception' => $e,
+                ]);
+
+                return null;
+            }
         }
 
         if (! \is_array($data) && ! \is_string($data)) {
